b399fc18add3f1a06d5f72ecea7b9f1f053bebea
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2020-07-18 21:17:58 +0200

    add articles to schema

--- resources/sql/content.sql
+++ resources/sql/content.sql
@@ -10,7 +10,7 @@ select id,
        updated_at,
        author_id
 from articles
-where slug = :slug
+where slug = :slug;
 
 -- A :result value of :n below will return affected rows:
 -- :name tags-for-article :? :*
@@ -18,5 +18,41 @@ where slug = :slug
 select name
 from tags t
          join article_tags a on t.id = a.tag_id
-where a.article_id = :id
+where a.article_id = :id;
+
+-- A :result value of :n below will return affected rows:
+-- :name article-count :? :1
+-- :doc Returns the total count of articles
+select count(*) count
+from articles a
+--~ (when (:filter params) "where")
+/*~ (when (-> params :filter :tag)
+  "exists (select 1 from tags t
+  join article_tags at on at.tag_id = t.id
+  where t.name = :filter.tag and at.article_id = a.id)")
+~*/
+;
+
+-- A :result value of :n below will return affected rows:
+-- :name articles :? :*
+-- :doc Returns the total count of articles
+select id,
+       slug,
+       title,
+       description,
+       body,
+       inserted_at,
+       updated_at,
+       author_id
+from articles a
+--~ (when (:filter params) "where")
+/*~ (when (-> params :filter :tag)
+  "exists (select 1 from tags t
+  join article_tags at on at.tag_id = t.id
+  where t.name = :filter.tag and at.article_id = a.id)")
+~*/
+order by updated_at, id DESC
+limit :page_size
+offset :offset
+
 
--- src/crg/schema.clj
+++ src/crg/schema.clj
@@ -7,7 +7,42 @@
     [com.stuartsierra.component :as component]
     [crg.db :as db]
     [clojure.edn :as edn]
-    ))
+    )
+  (:import java.util.Base64))
+
+(defn parse-int [s]
+  (Integer/parseInt (re-find #"\A-?\d+" s)))
+
+(defn encode [to-encode]
+  (.encodeToString (Base64/getEncoder) (.getBytes to-encode)))
+
+(defn decode [to-decode]
+  (String. (.decode (Base64/getDecoder) to-decode)))
+
+(defn article->edge
+  [[offset article]]
+  {:cursor (encode (str offset))
+   :node   article})
+
+(defn articles->article-edges
+  [articles start_offset end_offset]
+  (->> articles
+       (map vector (range start_offset end_offset))
+       (map article->edge)))
+
+(defn resolve-articles
+  [db]
+  (fn [_ args _]
+    (let [offset (if-let [cursor (:cursor args)]
+                   (parse-int (decode cursor))
+                   0)
+          total_count (dec ((db/article-count (:ds db) args) :count))
+          articles (db/articles (:ds db) (merge args {:offset offset}))
+          end_offset (+ offset (count articles))]
+      {:edges       (articles->article-edges articles offset end_offset)
+       :page_info   {:end_cursor    (encode (str end_offset))
+                     :has_next_page (< end_offset total_count)}
+       :total_count total_count})))
 
 (defn resolve-article-by-slug
   [db]
@@ -28,20 +63,21 @@
 (defn resolver-map
   [component]
   (let [db (:db component)]
-    {:query/article-by-slug (resolve-article-by-slug db)
+    {:query/article-by-slug    (resolve-article-by-slug db)
+     :query/articles           (resolve-articles db)
      :article/tags-for-article (resolve-tags-for-article db)}))
 
 (defn scalar-map
   []
   {
    :scalar/date-time-parse     serialze-date-time
-   :scalar/date-time-serialize  serialze-date-time
+   :scalar/date-time-serialize serialze-date-time
    })
 
 (defn enum-map
   []
-  {:sort_order {:parse identity
-   :serialize identity}})
+  {:sort_order {:parse     identity
+                :serialize identity}})
 
 (defn load-schema
   [component]
